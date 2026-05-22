a43b7aad6c9421b3f47522d48a133f9f35912c4b
Sangam Tiwari <sangamtiwari182@gmail.com>
2026-05-22 13:46:09 +0530

    Added product search functionality

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
 let productDiv = document.getElementById("products");
+let searchInput = document.getElementById("search");
+
+let allProducts = [];
+
 
 fetch("https://fakestoreapi.com/products")
-.then((res)=>{
+  .then((res) => {
     return res.json();
-})
+  })
 
-.then((data)=>{
+  .then((data) => {
+    allProducts = data;
 
-    data.forEach((item)=>{
+    displayProducts(allProducts);
+  });
 
-        let card = document.createElement("div");
 
-        card.className = "card";
+function displayProducts(products) {
 
-        card.innerHTML = `
-        
-        <img src="${item.image}">
-        
-        <h3>${item.title}</h3>
+  productDiv.innerHTML = "";
 
-        <p>Price : $${item.price}</p>
+  products.forEach((item) => {
 
-        <button>Add to Cart</button>
-        
-        `;
+    let card = document.createElement("div");
 
-        productDiv.appendChild(card);
+    card.className = "card";
 
-    });
+    card.innerHTML = `
+    
+      <img src="${item.image}">
+      
+      <h3>${item.title}</h3>
+
+      <p>Price : $${item.price}</p>
+
+      <p>${item.category}</p>
+
+      <button>Add to Cart</button>
+    
+    `;
+
+    productDiv.appendChild(card);
+  });
+}
+
+searchInput.addEventListener("input", () => {
+
+  let searchValue = searchInput.value.toLowerCase();
+
+  let filteredProducts = allProducts.filter((item) => {
+
+    return item.title.toLowerCase().includes(searchValue);
+
+  });
+
+  displayProducts(filteredProducts);
 
 });
